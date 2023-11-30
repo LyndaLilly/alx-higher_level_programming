72f1a431b5610ca788427fc80a2c5ce1aea7d366
LyndaLilly <lilianlynda22@gmal.com>
2023-11-30 06:08:26 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     x = 1
     y = 2
-    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
+print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
