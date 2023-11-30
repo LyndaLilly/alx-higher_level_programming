8b1dd0ae8a41f84e1ae8da82de30a57179eb3b9d
LyndaLilly <lilianlynda22@gmal.com>
2023-11-30 07:00:15 -0800

    task 8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+
+def adds(a, b):
+    if a < b:
+        c = add(a, b)
+        for m in range(4, 6):
+            y = add(y, m)
+        return (y)
+    else:
+        return sub(a, b)
