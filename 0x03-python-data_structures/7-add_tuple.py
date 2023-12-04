2d4b3703583e787028d2ef7d6c615daf89fa220a
LyndaLilly <lilianlynda22@gmal.com>
2023-12-04 03:18:03 -0800

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     x, y = len(tuple_a), len(tuple_b)
     z = ((tuple_a[0] if x >= 1 else 0) +
-            (tuple_b[0] if y >= 1 else 0),
-            (tuple_a[1] if x >= 2 else 0) +
-            (tuple_b[1] if y >= 2 else 0))
+        (tuple_b[0] if y >= 1 else 0),
+        (tuple_a[1] if x >= 2 else 0) +
+        (tuple_b[1] if y >= 2 else 0))
     return z
