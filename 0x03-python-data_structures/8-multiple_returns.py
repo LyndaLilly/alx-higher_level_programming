890e3da93491edd24c4b2209ab5db088a3edac28
LyndaLilly <lilianlynda22@gmal.com>
2023-12-04 02:59:29 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    res1 = len(sentence)
-    res2 = sentence[0] if res1 > 0 else "None"
-    res3 = length, res2
-    return(res3)
+    if not sentence:
+        sentence = None
+    if sentence:
+    x = len(sentence)
+    else:
+        x = 0
+    return (x, sentence if not sentence else sentence[:1])
