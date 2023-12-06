791c2e0069614f53113927d30374470ec6979155
LyndaLilly <lilianlynda22@gmal.com>
2023-12-06 10:17:20 -0800

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
-def roman_to_int(roman_string:str):
-    if roman_string is None or type(roman_string) != str:
+def roman_to_int(roman_string):
+    r = ""
+    if roman_string is None or type(roman_string) != r:
         return 0
     lists = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = [lists[p] for p in roman_string] + [0]
