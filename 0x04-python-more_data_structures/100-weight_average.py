dec4563a401e624e994f7c3ce17b17af57d87764
LyndaLilly <lilianlynda22@gmal.com>
2023-12-08 07:08:40 -0800

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
+
         return 0
 
     x = 0
     y = 0
 
-    for z in my_list:
-        x += z[0] * z[1]
-        y += z[1]
+    for i in my_list:
+        x += i[0] * i[1]
+        y += i[1]
 
-    return (x/ y)
+    return (x/y)
