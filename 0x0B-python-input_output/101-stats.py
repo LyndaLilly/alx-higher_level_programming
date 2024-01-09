e60b7c0161216546ab15222568a24a80bd6a1246
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-01-09 05:36:51 -0800

    task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-"""input and computes metrics.
+"""gets stdin and computes metrics.
 
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
@@ -11,13 +10,15 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    for x in sorted(status_codes):
+        print("{}: {}".format(x, status_codes[x]))
+
 
 if __name__ == "__main__":
     import sys
@@ -28,26 +29,26 @@ if __name__ == "__main__":
     cnt = 0
 
     try:
-        for ln in sys.stdin:
+        for y in sys.stdin:
             if cnt == 10:
                 print_stats(size, status_codes)
                 cnt = 1
             else:
                 cnt += 1
 
-            ln = ln.split()
+            y = y.split()
 
             try:
-                size += int(ln[-1])
+                size += int(y[-1])
             except (IndexError, ValueError):
                 pass
 
             try:
-                if ln[-2] in valid_codes:
-                    if status_codes.get(ln[-2], -1) == -1:
-                        status_codes[ln[-2]] = 1
+                if y[-2] in valid_codes:
+                    if status_codes.get(y[-2], -1) == -1:
+                        status_codes[y[-2]] = 1
                     else:
-                        status_codes[ln[-2]] += 1
+                        status_codes[y[-2]] += 1
             except IndexError:
                 pass
 
