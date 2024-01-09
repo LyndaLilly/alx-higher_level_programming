0856053019bc184273cbf6f3dfed8a36688350fe
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-01-09 05:51:59 -0800

    task 8

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,12 +1,12 @@
 #include "Python.h"
 
 /**
- * print_python_string - this prints the python string.
- * @p: python string to be printed.
+ * print_python_string - this prints python string.
+ * @p: string object to print.
  */
 void print_python_string(PyObject *p)
 {
-	long int wt;
+	long int length;
 
 	fflush(stdout);
 
@@ -17,12 +17,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	wt = ((PyASCIIObject *)(p))->wt;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", wt);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &wt));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
