a905635a492020acea5c5de34dca296c41e35b9a
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-03-18 07:58:21 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-this shows all entries in the state table of hbtn_0e_0_usa where name is provided.
+this accept an argument to show all entries in
+"states" table of hbtn_0e_0_usa where name hits the provided.
 """
 
 import MySQLdb
@@ -10,11 +11,11 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    rc = db.cursor()
+    rc.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                 .format(sys.argv[4]))
-    rows = cur.fetchall()
+    rows = rc.fetchall()
     for row in rows:
         print(row)
-    cur.close()
+    rc.close()
     db.close()
