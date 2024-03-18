8b4479d398bfd25024c6f6cf5a32ef87ec913cb8
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-03-18 07:17:28 -0700

    tasks

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this displays code in database"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], database=argv[3])
+    rc = database.rc()
+    rc.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for s in rc.fetchall():
+        print()
+    rc.close()
+    database.close()
