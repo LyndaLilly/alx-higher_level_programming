4c8aae6f2b2c050ba5c815d08429ef4de3ddf6e4
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-03-29 04:57:00 -0700

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,8 @@ from sys import argv
 if __name__ == '__main__':
     link = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     ans = requests.get(link)
-    infos = ans.json()
-    for xy in infos[:10]:
-        print(xy.get('sha'), end=': ')
-        print(xy.get('xy').get('author').get('name'))
+    commits = ans.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
+
