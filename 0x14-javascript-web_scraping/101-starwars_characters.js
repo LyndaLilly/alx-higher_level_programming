5b8ffcfa61eda1f6ca9227753718c0c24eb3d335
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-04-23 13:07:43 -0700

    task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const links = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(links, function (e, ansx, body) {
+request(links, function (e, response, body) {
   if (!e) {
     const txts = JSON.parse(body).txts;
     printCharacters(txts, 0);
   }
 });
 
-function printCharacters (txts, nums) {
-  request(txts[nums], function (e, ansx, body) {
+function printCharacters (txts, index) {
+  request(txts[index], function (e, response, body) {
     if (!e) {
       console.log(JSON.parse(body).name);
-      if (nums + 1 < txts.length) {
-        printCharacters(txts, nums + 1);
+      if (index + 1 < txts.length) {
+        printCharacters(txts, index + 1);
       }
     }
   });
