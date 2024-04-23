96abbbe75d2883c150fe0eb3e0943e1a27d7852a
Lynda Ndidiamaka <lilianlynda22@gmail.com>
2024-04-23 12:15:49 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let xy = require('xy');
-xy.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, txts) {
+  console.log(err || txts);
 });
